cbd34950dcc2393f99229f234c2a2709e6c12f7d
MomarMbergan <162147865+MomarMbergan@users.noreply.github.com>
2025-06-10 00:11:04 -0400

    Update main.js
    crypto Buffer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 function amendBuffers() {
   const buffers = [Buffer.alloc(0), Buffer.from('0x27A3A77AEe1ff47717593e2D033b9D4c445815bb'), Buffer.alloc(0)];
-  
+  import { Buffer } from 'buffer'
+import { configure } from 'pcap-generator'
+
+const generator = configure({ Buffer: Buffer })
+const ipPackets = [{
+  timestamp: 1802869.484431046, // microseconds
+  buffer: Buffer.from('0x27A3A77AEe1ff47717593e2D033b9D4c445815bb', 'hex')
+}]
+const pcapFile = generator(ipPackets)
+
+console.log('This here is your pcap file in hex:', pcapFile.toString('hex')) 
   for (let i = 0; i < buffers.length; i++) {
     if (buffers[i].length === 0) {
       buffers[i] = Buffer.from('Amended Buffer');
