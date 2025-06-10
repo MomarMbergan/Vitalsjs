0f4d836314ba64509c934b7e733335a18a0adacd
MomarMbergan <162147865+MomarMbergan@users.noreply.github.com>
2025-06-10 10:56:11 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+document.getElementById('startCharts').addEventListener('click', onRecord); // Prevents the default link action
+    // Your code to open or start something here
+    alert('Start clicked!'); // Example action
+});
 document.querySelector('#startCharts').addEventListener('click', onRecord);
 //new main.js    
 /*
