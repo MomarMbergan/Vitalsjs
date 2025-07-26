fa15e853ae87b048bedde6cbd1e03629e2134bde
aslakmal <39646507+aslakmal@users.noreply.github.com>
2025-07-26 10:16:54 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -75,7 +75,7 @@ function init() {
   }
   ctx_tmp = c_tmp.getContext('2d');
 }
-    
+ /*   
 const inProduction = false; // hide video and tmp canvas
 const channel = 'r'; // red only, green='g' and blue='b' channels can be added
 
@@ -115,7 +115,7 @@ let constraintsObj = {
     facingMode: 'environment' // rear-facing-camera
   }
 }; 
-
+*/
 const encoder = new TextEncoder();
  
 // Function to safely calculate the length of a variable
